a4aae8ab5ca4a1f66722f30256c7cbd4908fe325
dafeng012 <111509125+dafeng012@users.noreply.github.com>
2022-09-05 00:48:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -26,20 +26,6 @@ export const config = {
    */
   USERS: [
     {
-      // 想要发送的人的名字
-      name: "老婆1",
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "of5bp580wfz7Eve0a5BBZMQUcXlY",
-      // 你想对他发送的模板消息的模板ID
-      useTemplateId: "5km6sSrxWKzQrIFGy8hitL3_TUBgp51ZVT4rFesVbfg",
-      // 所在省份
-      province: "广东",
-      // 所在城市
-      city: "肇庆",
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
-    }, 
-    {
       name: "老婆1", 
       id: "of5bp580wfz7Eve0a5BBZMQUcXlY",
       useTemplateId: "fDp1KNEW4O3dnDdGy341truJhIeM-XFXkvebpesRxxs",
